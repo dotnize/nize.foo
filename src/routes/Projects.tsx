98300b10d7998a7270c9af242d12ddb097986c6b
nize <contact@nize.ph>
2022-08-08 16:34:39 +0800

    update project urls

--- src/routes/Projects.tsx
+++ src/routes/Projects.tsx
@@ -4,18 +4,18 @@ import { For } from 'solid-js';
 const projects = [
   {
     name: 'Dodong',
-    url: 'https://github.com/nizeic/Dodong',
+    url: 'https://github.com/nizewn/Dodong',
     description: 'A music bot for the Discord chat platform built with discord.js and Node.'
   },
   {
     name: 'moodle-scrape',
-    url: 'https://github.com/nizeic/moodle-scrape',
+    url: 'https://github.com/nizewn/moodle-scrape',
     description:
       'A simple Node.js package for scraping student data and resources from Moodle LMS sites.'
   },
   {
     name: 'chessu',
-    url: 'https://github.com/nizeic/chessu',
+    url: 'https://github.com/nizewn/chessu',
     description:
       'An online multiplayer chess site built on React and Express/Node.js with socket.io.'
   }
