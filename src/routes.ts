42f23841d79625a0aa94621eee2effef58d2b17b
Nathaniel Tampus <contact@nize.ph>
2022-10-31 19:49:28 +0800

    lazyload 404 component

--- src/routes.ts
+++ src/routes.ts
@@ -1,12 +1,11 @@
 import { lazy } from 'solid-js';
-import NotFound from './routes/404';
 import Home from './routes/Home';
 import Projects from './routes/Projects';
 
 const routes = [
   {
     path: '*',
-    component: NotFound
+    component: lazy(() => import('./routes/404'))
   },
   {
     path: '/',
