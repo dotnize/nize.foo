e17e12aa40b4e7af8ef8232dc04596cbbf5d367a
Nathaniel Tampus <contact@nize.ph>
2022-10-31 19:51:46 +0800

    use Anchor component

--- src/components/Nav/Nav.tsx
+++ src/components/Nav/Nav.tsx
@@ -1,20 +1,20 @@
 import type { Component } from 'solid-js';
-import { Link } from '@solidjs/router';
+import { A } from '@solidjs/router';
 
 import './Nav.css';
 
 const Nav: Component = () => {
   return (
     <nav class="space-bottom">
-      <Link href="/" title="home" class="navlink space-right">
+      <A href="/" title="home" class="navlink space-right">
         home.txt
-      </Link>
-      <Link href="/projects" title="projects" class="navlink space-right">
+      </A>
+      <A href="/projects" title="projects" class="navlink space-right">
         projects.txt
-      </Link>
-      <Link href="/chat" title="chat" class="navlink">
+      </A>
+      <A href="/chat" title="chat" class="navlink">
         chat.sh
-      </Link>
+      </A>
     </nav>
   );
 };
