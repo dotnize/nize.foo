e032a15e8fe7c95788bc818d6794b1b24e21f809
Nathaniel Tampus <contact@nize.ph>
2022-10-31 18:24:32 +0800

    explicit size for footer images

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -12,19 +12,19 @@ const Footer: Component = () => {
   return (
     <footer>
       <a href="mailto:contact@nize.ph" title="Email">
-        <img src={mail} alt="Email" />
+        <img src={mail} alt="Email" width={25} height={25} />
       </a>
       <a href="https://github.com/nizewn" title="GitHub" target="_blank">
-        <img src={github} alt="GitHub" />
+        <img src={github} alt="GitHub" width={25} height={25} />
       </a>
       <a href="https://gitlab.com/nizewn" title="GitLab" target="_blank">
-        <img src={gitlab} alt="GitLab" />
+        <img src={gitlab} alt="GitLab" width={25} height={25} />
       </a>
       <a href="https://linkedin.com/in/nize" title="LinkedIn" target="_blank">
-        <img src={linkedin} alt="LinkedIn" />
+        <img src={linkedin} alt="LinkedIn" width={25} height={25} />
       </a>
       <a href="https://twitter.com/nizewn" title="Twitter" target="_blank">
-        <img src={twitter} alt="Twitter" />
+        <img src={twitter} alt="Twitter" width={25} height={25} />
       </a>
       <br />
       nize &#169; 2022
