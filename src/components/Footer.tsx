a97c715f17c99dbad311688fdee4706028e46aad
Nathaniel Tampus <contact@nize.ph>
2023-04-08 01:39:19 +0800

    thinner fake caret for less distractions

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,6 +1,6 @@
 export default function Footer() {
     return (
-        <footer class="blink mt-8 flex w-fit items-center gap-2 border-r-[10px] border-gruvbox-fg font-azeret text-sm motion-safe:animate-blink dark:border-gruvboxDark-fg">
+        <footer class="blink mt-8 flex w-fit items-center gap-2 border-r-2 border-gruvbox-fg font-azeret text-sm motion-safe:animate-blink dark:border-gruvboxDark-fg">
             <span class="text-gruvbox-aqua dark:text-gruvboxDark-aqua2">&gt;</span>
             <a href="mailto:contact@nize.ph" class="hover:underline" title="Email">
                 contact@nize.ph
