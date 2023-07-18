6a410a632e0b4b612a2edff95595992f441109af
Nathaniel Tampus <contact@nize.foo>
2023-07-18 14:28:19 +0800

    update site description

--- src/consts.ts
+++ src/consts.ts
@@ -1,6 +1,5 @@
 export const SITE_TITLE = "nize";
-export const SITE_DESCRIPTION =
-    "A developer from the Philippines with a passion for full-stack web and game development.";
+export const SITE_DESCRIPTION = "A developer from the Philippines.";
 
 export interface ProjectProps {
     name: string;
