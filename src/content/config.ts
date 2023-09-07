a07b247e3193b12f5337b9f4f16c4c19fa51eee6
Nathaniel Tampus <nathaniel@n9ze.com>
2023-09-07 21:03:07 +0800

    fix formatting

--- src/content/config.ts
+++ src/content/config.ts
@@ -7,7 +7,7 @@ const blog = defineCollection({
         description: z.string(),
         // Transform string to Date object
         pubDate: z.coerce.date(),
-		updatedDate: z.coerce.date().optional(),
+        updatedDate: z.coerce.date().optional(),
         heroImage: z.string().optional(),
     }),
 });
