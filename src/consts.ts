374afc308a26f6cf08d2b9dbd7f2bd4573e7b4bb
Nathaniel Tampus <nate@mugnavo.com>
2024-01-15 20:28:10 +0800

    format

--- src/consts.ts
+++ src/consts.ts
@@ -2,16 +2,16 @@ export const SITE_TITLE = "nize";
 export const SITE_DESCRIPTION = "A developer from the Philippines.";
 
 export interface ProjectProps {
-    name: string;
-    description: string;
-    stack: string;
-    repo: string;
-    demo?: string;
+  name: string;
+  description: string;
+  stack: string;
+  repo: string;
+  demo?: string;
 }
 
 export interface LayoutProps {
-    title?: string;
-    description?: string;
-    image?: string;
-    children: any;
+  title?: string;
+  description?: string;
+  image?: string;
+  children: any;
 }
--- src/pages/msg.ts
+++ src/pages/msg.ts
@@ -3,47 +3,47 @@ export const prerender = false;
 import { type APIRoute } from "astro";
 
 export const POST: APIRoute = async ({ request }) => {
-    try {
-        if (request.headers.get("Content-Type") !== "application/json") {
-            return new Response("Invalid request", { status: 400 });
-        }
-
-        const msg = (await request.json()) as {
-            title?: string;
-            text?: string;
-            contact?: string;
-        };
-        if (!msg || !msg.text) {
-            return new Response("Invalid request body", { status: 400 });
-        }
-
-        const res = await fetch(import.meta.env.DISCORD_WEBHOOK_URL, {
-            method: "POST",
-            headers: {
-                "Content-Type": "application/json",
-            },
-            body: JSON.stringify({
-                embeds: [
-                    {
-                        title: msg.title,
-                        description: msg.text,
-                        footer: msg.contact
-                            ? {
-                                  text: "👤 " + msg.contact,
-                              }
-                            : undefined,
-                    },
-                ],
-            }),
-        });
-
-        if (res && res.ok) {
-            return new Response("Sent!", { status: 200 });
-        } else {
-            return new Response("Message not sent", { status: 500 });
-        }
-    } catch (err) {
-        console.error(err);
-        return new Response("Internal server error", { status: 500 });
+  try {
+    if (request.headers.get("Content-Type") !== "application/json") {
+      return new Response("Invalid request", { status: 400 });
     }
+
+    const msg = (await request.json()) as {
+      title?: string;
+      text?: string;
+      contact?: string;
+    };
+    if (!msg || !msg.text) {
+      return new Response("Invalid request body", { status: 400 });
+    }
+
+    const res = await fetch(import.meta.env.DISCORD_WEBHOOK_URL, {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({
+        embeds: [
+          {
+            title: msg.title,
+            description: msg.text,
+            footer: msg.contact
+              ? {
+                  text: "👤 " + msg.contact,
+                }
+              : undefined,
+          },
+        ],
+      }),
+    });
+
+    if (res && res.ok) {
+      return new Response("Sent!", { status: 200 });
+    } else {
+      return new Response("Message not sent", { status: 500 });
+    }
+  } catch (err) {
+    console.error(err);
+    return new Response("Internal server error", { status: 500 });
+  }
 };
