d2c94e89f3133aed9d387fa23c07786935bdab0d
Nathaniel Tampus <nate@mugnavo.com>
2024-01-30 00:48:20 +0800

    update homepage and card layouts

--- src/consts.ts
+++ src/consts.ts
@@ -1,14 +1,6 @@
 export const SITE_TITLE = "nize";
 export const SITE_DESCRIPTION = "A developer from the Philippines.";
 
-export interface ProjectProps {
-  name: string;
-  description: string;
-  stack: string;
-  repo: string;
-  demo?: string;
-}
-
 export interface LayoutProps {
   title?: string;
   description?: string;
