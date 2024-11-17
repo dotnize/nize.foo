15ec6f421f2e9c50402f730f09883415451cc9f9
Nathaniel Tampus <nate@mugnavo.com>
2024-11-17 12:15:00 +0800

    wip

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,20 +4,26 @@ import sitemap from "@astrojs/sitemap";
 import solidJs from "@astrojs/solid-js";
 import tailwind from "@astrojs/tailwind";
 
-import vercel from "@astrojs/vercel/serverless";
+import vercel from "@astrojs/vercel";
+
+import mdx from "@astrojs/mdx";
 
 // https://astro.build/config
 export default defineConfig({
+  markdown: {
+    shikiConfig: { themes: { light: "vitesse-light", dark: "vitesse-dark" } },
+  },
   site: "https://nize.foo",
   integrations: [
+    mdx(),
     sitemap(),
     tailwind({
       applyBaseStyles: false,
     }),
     solidJs(),
   ],
-  output: "hybrid",
-  adapter: vercel({ functionPerRoute: false }),
+  output: "static",
+  adapter: vercel(),
   prefetch: {
     prefetchAll: true,
     defaultStrategy: "hover",
